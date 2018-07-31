31720b74a60ecbfe5bdeafd328cb34bacef0dc9d
Alex Hamlin <alex@alexhamlin.co>
2018-07-30 19:50:23 -0700

    Minor error handling improvements
    This is just practice for being able to run this on the CLI or on
    Lambda.

--- cmd/randomize/main.go
+++ cmd/randomize/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -10,8 +11,20 @@ import (
 func main() {
 	result, err := randomizer.Main(os.Args[1:])
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	switch err {
+	case randomizer.ErrTooFewOptions:
+		fmt.Fprintln(os.Stderr, "Hey, I need things to randomize!")
+		os.Exit(1)
+
+	case flag.ErrHelp:
+		fmt.Fprintln(os.Stderr, "Usage: Just give me things to randomize!")
+		return
+
+	default:
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 
 	fmt.Println(result)
--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package randomizer
 import (
 	"errors"
 	"flag"
+	"io/ioutil"
 	"math/rand"
 	"time"
 )
+
+// ErrTooFewOptions is returned when, after parsing arguments, there are fewer
+// than two options to choose from for randomization.
+var ErrTooFewOptions = errors.New("nothing to randomize")
 
 // Main is the entrypoint to the randomizer tool.
 func Main(args []string) (string, error) {
@@ -17,7 +22,7 @@ func Main(args []string) (string, error) {
 
 	options := fs.Args()
 	if len(options) < 2 {
-		return "", errors.New("nothing to randomize")
+		return "", ErrTooFewOptions
 	}
 
 	source := rand.NewSource(time.Now().UnixNano())
@@ -34,6 +39,7 @@ func buildFlagSet() *flagSet {
 	fs := &flagSet{
 		FlagSet: flag.NewFlagSet("randomizer", flag.ContinueOnError),
 	}
+	fs.SetOutput(ioutil.Discard)
 
 	return fs
 }
