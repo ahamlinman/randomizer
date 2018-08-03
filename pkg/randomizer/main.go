0bb1cd7af59197ddf271889393d8bc4dd1d76ea1
Alex Hamlin <alex@alexhamlin.co>
2018-08-02 21:01:34 -0700

    Add note about selection weighting
    Just to be perfectly clear.

--- pkg/randomizer/main.go
+++ pkg/randomizer/main.go
@@ -198,7 +198,10 @@ You can also save *groups* for later use.
 *List groups:* {{.Name}} -list
 *Show options in a group:* {{.Name}} -show first3
 *Delete a group:* {{.Name}} -delete first3
-`))
+
+Note that the selection is weighted. An option is more likely to be picked if
+it is given multiple times. This also applies when multiple groups are given,
+and an option is in more than one of them.`))
 
 func (a *App) buildUsage() string {
 	var buf bytes.Buffer
