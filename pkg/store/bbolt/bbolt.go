dd1d9ed827fe68ab3c760368f880b47473bc90f9
Alex Hamlin <alex@alexhamlin.co>
2018-08-06 20:33:44 -0700

    Don't just hardcode the Bolt bucket name
    It's fun to overengineer like this.

--- pkg/store/bbolt/bbolt.go
+++ pkg/store/bbolt/bbolt.go
@@ -8,24 +8,45 @@ import (
 	"github.com/pkg/errors"
 )
 
-const groupsBucket = "groups"
+// DefaultBucket is the default bucket in which groups are stored in the bbolt
+// database.
+const DefaultBucket = "groups"
 
 // Store is a store backed by a bbolt database.
 type Store struct {
-	db *bolt.DB
+	db     *bolt.DB
+	bucket string
 }
 
+// Option represents a type for options that can be applied to a Store.
+type Option func(*Store)
+
 // New creates a new store backed by the provided (pre-opened) bbolt database.
-func New(db *bolt.DB) *Store {
-	return &Store{
-		db: db,
+func New(db *bolt.DB, options ...Option) *Store {
+	store := &Store{
+		db:     db,
+		bucket: DefaultBucket,
+	}
+
+	for _, opt := range options {
+		opt(store)
+	}
+
+	return store
+}
+
+// WithBucketName creates a Store that stores groups in the named bucket in the
+// bbolt database, rather than the default "groups" bucket.
+func WithBucketName(name string) Option {
+	return func(b *Store) {
+		b.bucket = name
 	}
 }
 
 // List obtains the set of stored groups.
 func (b *Store) List() (groups []string, err error) {
 	err = b.db.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(groupsBucket))
+		bucket := tx.Bucket([]byte(b.bucket))
 		if bucket == nil {
 			return nil
 		}
@@ -41,7 +62,7 @@ func (b *Store) List() (groups []string, err error) {
 // Get obtains the options in a single named group.
 func (b *Store) Get(name string) (options []string, err error) {
 	err = b.db.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(groupsBucket))
+		bucket := tx.Bucket([]byte(b.bucket))
 		if bucket == nil {
 			return errors.Errorf("group %q does not exist", name)
 		}
@@ -60,7 +81,7 @@ func (b *Store) Get(name string) (options []string, err error) {
 // Put saves the provided options into a named group.
 func (b *Store) Put(name string, options []string) error {
 	return b.db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists([]byte(groupsBucket))
+		bucket, err := tx.CreateBucketIfNotExists([]byte(b.bucket))
 		if err != nil {
 			return errors.Wrap(err, "creating bucket")
 		}
@@ -82,7 +103,7 @@ func (b *Store) Put(name string, options []string) error {
 // Delete removes the named group from the store.
 func (b *Store) Delete(name string) error {
 	return b.db.Update(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(groupsBucket))
+		bucket := tx.Bucket([]byte(b.bucket))
 		if bucket == nil {
 			return errors.Errorf("group %q does not exist", name)
 		}
