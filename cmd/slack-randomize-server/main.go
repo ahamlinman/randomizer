5e51103d887e0f752effc83cbdd497ce2b5b114a
Alex Hamlin <alex@alexhamlin.co>
2018-08-06 21:16:55 -0700

    Create a basic randomizer server for Slack
    This uses the same backend as the randomizer app, but with a new
    Slack-powerable frontend.
    
    With this it's possible to basically get all the functionality of my
    original Slack randomizer slash command.

---
+++ cmd/slack-randomize-server/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/ahamlinman/randomizer/pkg/randomizer"
+	boltstore "github.com/ahamlinman/randomizer/pkg/store/bbolt"
+	bolt "github.com/coreos/bbolt"
+	"github.com/pkg/errors"
+)
+
+type slackResponse struct {
+	ResponseType string `json:"response_type"`
+	Text         string `json:"text"`
+}
+
+func (s slackResponse) Send(w io.Writer) {
+	json.NewEncoder(w).Encode(&s)
+}
+
+func main() {
+	token := os.Getenv("SLACK_TOKEN")
+	if token == "" {
+		fmt.Fprintln(os.Stderr, "SLACK_TOKEN must be provided")
+		os.Exit(2)
+	}
+
+	db, err := bolt.Open("randomizer.db", os.ModePerm&0644, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(2)
+	}
+
+	app := randomizer.NewApp("/randomize", boltstore.New(db))
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseForm(); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		reqToken := r.PostForm.Get("token")
+		// TODO: Vulnerable to timing attacks, if it actually matters
+		// (I might fix it just for fun)
+		if reqToken != token {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+
+		result, err := app.Main(strings.Split(r.PostForm.Get("text"), " "))
+		if err != nil {
+			slackResponse{"ephemeral", err.(randomizer.Error).HelpText()}.Send(w)
+			return
+		}
+
+		switch result.Type() {
+		case randomizer.ListedGroups, randomizer.ShowedGroup:
+			slackResponse{"ephemeral", result.Message()}.Send(w)
+
+		default:
+			slackResponse{"in_channel", result.Message()}.Send(w)
+		}
+	}
+
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Starting randomizer service")
+	err = http.ListenAndServe("0.0.0.0:7636", nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", errors.Wrap(err, "starting server"))
+		os.Exit(1)
+	}
+}
