1a33276a2217a607b886c67b3c5e8bf13ae5bdf5
Alex Hamlin <alex@alexhamlin.co>
2018-09-22 20:50:26 -0700

    Use existing Slack HTTP implementation for Lambda
    Turns out, AWS Labs has a nice little package that proxies API Gateway
    request and response models into standard Go HTTP requests and
    responses. And it was super easy to integrate into my handler.
    
    Originally I regretted moving HTTP handling logic into the Slack
    package, but honestly formatting the response codes and setting headers
    correctly and whatnot is hard enough that I might just make that the
    only public interface.

--- cmd/slack-lambda-handler/main.go
+++ cmd/slack-lambda-handler/main.go
@@ -2,11 +2,11 @@ package main // import "go.alexhamlin.co/randomizer/cmd/slack-lambda-handler"
 
 import (
 	"errors"
-	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/handlerfunc"
 
 	"go.alexhamlin.co/randomizer/pkg/slack"
 	"go.alexhamlin.co/randomizer/pkg/store"
@@ -42,30 +42,5 @@ func handleEvent(request events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 		StoreFactory: storeFactory,
 	}
 
-	result, err := app.Run(slack.Request{
-		Token:     request.QueryStringParameters["token"],
-		SSLCheck:  request.QueryStringParameters["ssl_check"],
-		ChannelID: request.QueryStringParameters["channel_id"],
-		Text:      request.QueryStringParameters["text"],
-	})
-
-	if err == slack.ErrIncorrectToken {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusForbidden,
-		}, nil
-	} else if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if result == (slack.Response{}) {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusOK,
-		}, nil
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusOK,
-		Headers:    map[string]string{"Content-Type": "application/json"},
-		Body:       result.String(),
-	}, nil
+	return handlerfunc.New(app.ServeHTTP).Proxy(request)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go.alexhamlin.co/randomizer
 require (
 	github.com/aws/aws-lambda-go v1.6.0
 	github.com/aws/aws-sdk-go-v2 v2.0.0-preview.4+incompatible
+	github.com/awslabs/aws-lambda-go-api-proxy v0.0.0-20180713153304-3a4d35011e4a
 	github.com/go-ini/ini v1.38.2 // indirect
 	github.com/jmespath/go-jmespath v0.0.0-20180206201540-c2b33e8439af // indirect
 	github.com/pkg/errors v0.8.0
