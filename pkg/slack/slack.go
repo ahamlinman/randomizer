35b50729c1c3f94b32cf423bf387d296863b082e
Alex Hamlin <alex@alexhamlin.co>
2018-09-22 21:26:12 -0700

    Make Slack package do HTTP handling only
    I thought that the additional abstraction between HTTP and responses to
    Slack would help ease the creation of the Lambda integration. In
    reality, it turned out that figuring out how to map the model I had
    created back to HTTP was also non-trivial, and duplicating that between
    the normal HTTP server and the Lambda handler for their different types
    of integrations was not really worth it. Much easier to write that code
    once in a standard HTTP handler and make an external package map that
    back to API Gateway's expected format.

--- pkg/slack/slack.go
+++ pkg/slack/slack.go
@@ -1,7 +1,6 @@
 package slack // import "go.alexhamlin.co/randomizer/pkg/slack"
 
 import (
-	"bytes"
 	"crypto/subtle"
 	"encoding/json"
 	"fmt"
@@ -10,131 +9,27 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/pkg/errors"
-
 	"go.alexhamlin.co/randomizer/pkg/randomizer"
 	"go.alexhamlin.co/randomizer/pkg/store"
 )
 
-// Request represents a request for a Slack slash command. Its values can be
-// obtained from query string parameters or the request body, depending on the
-// configuration of the command in Slack.
-type Request struct {
-	Token     string
-	SSLCheck  string
-	ChannelID string
-	Text      string
-}
-
-// ResponseType represents the manner in which a response to a Slack slash
-// command request will be displayed to a user.
-type ResponseType int
-
-const (
-	// TypeEphemeral causes a response to be displayed to the user only. The
-	// slash command invocation will be hidden from others.
-	TypeEphemeral ResponseType = iota + 1
-
-	// TypeInChannel causes a response to be displayed in the channel to other
-	// Slack users, along with the slash command invocation that triggered it.
-	TypeInChannel
-)
-
-// MarshalText encodes a ResponseType into the textual representation
-// understood by the Slack API.
-func (t ResponseType) MarshalText() ([]byte, error) {
-	switch t {
-	case TypeEphemeral:
-		return []byte("ephemeral"), nil
-
-	case TypeInChannel:
-		return []byte("in_channel"), nil
-	}
-
-	panic(fmt.Errorf("unknown response type code %v", t))
-}
-
-// Response represents a response to a Slack slash command.
-type Response struct {
-	Type ResponseType `json:"response_type"`
-	Text string       `json:"text"`
-}
-
-// Send writes the JSON form of a Response to the provided writer. If the
-// Response is empty, nothing will be written.
-func (r Response) Send(w io.Writer) {
-	if r == (Response{}) {
-		return
-	}
-
-	json.NewEncoder(w).Encode(&r)
-}
-
-// String returns the JSON form of a Response. If the Response is empty, the
-// string will be empty.
-func (r Response) String() string {
-	var buf bytes.Buffer
-	r.Send(&buf)
-	return buf.String()
-}
-
-// ErrIncorrectToken indicates that the authentication token provided in the
-// request did not match the expected value.
-var ErrIncorrectToken = errors.New("invalid authentication token")
-
-// App represents a randomizer app that interacts with the Slack slash command
-// API. It provides functionality for validating authentication tokens and
-// returning responses in Slack's expected formats.
+// App provides HTTP handling logic that allows the randomizer to be integrated
+// as a slash command in a Slack workspace.
 type App struct {
-	// Name is the name of the command as displayed in help output.
+	// Name is the name of the command as displayed in help output. Ideally, it
+	// should match the name of the slash command configured in Slack.
 	Name string
-	// Token is the expected value of the Slack authentication token.
+	// Token is the expected value of the authentication token provided by Slack.
+	// This can be obtained from the slash command configuration.
 	Token []byte
 	// StoreFactory provides a Store for the Slack channel in which the request
 	// was made.
 	StoreFactory store.Factory
 }
 
-// Run processes a slash command request from Slack and returns an appropriate
-// response. If the request token is invalid, ErrIncorrectToken will be
-// returned.
-func (a App) Run(r Request) (Response, error) {
-	// This function "[requires] careful thought to use correctly." So hopefully
-	// I managed to do that.
-	if subtle.ConstantTimeCompare(a.Token, []byte(r.Token)) != 1 {
-		return Response{}, ErrIncorrectToken
-	}
-
-	if r.SSLCheck == "1" {
-		return Response{}, nil
-	}
-
-	app := randomizer.NewApp(a.Name, a.StoreFactory(r.ChannelID))
-	result, err := app.Main(strings.Split(r.Text, " "))
-	if err != nil {
-		return Response{
-			Type: TypeEphemeral,
-			Text: err.(randomizer.Error).HelpText(),
-		}, nil
-	}
-
-	switch result.Type() {
-	case randomizer.ListedGroups, randomizer.ShowedGroup:
-		return Response{
-			Type: TypeEphemeral,
-			Text: result.Message(),
-		}, nil
-
-	default:
-		return Response{
-			Type: TypeInChannel,
-			Text: result.Message(),
-		}, nil
-	}
-}
-
-// ServeHTTP allows an App to be directly used as a HTTP handler. It supports
-// both GET and POST modes for a Slack slash command integration.
+// ServeHTTP handles the GET or POST request made by Slack when the randomizer
+// slash command is invoked. (The HTTP method used by Slack can be selected
+// when configuring the slash command.)
 func (a App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var params url.Values
 	if r.Method == "POST" {
@@ -147,24 +42,62 @@ func (a App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		params = r.URL.Query()
 	}
 
-	response, err := a.Run(Request{
-		Token:     params.Get("token"),
-		SSLCheck:  params.Get("ssl_check"),
-		ChannelID: params.Get("channel_id"),
-		Text:      params.Get("text"),
-	})
-
-	if err != nil {
-		if err == ErrIncorrectToken {
-			w.WriteHeader(http.StatusForbidden)
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
+	// This function "[requires] careful thought to use correctly." So hopefully
+	// I managed to do that.
+	if subtle.ConstantTimeCompare(a.Token, []byte(params.Get("token"))) != 1 {
+		w.WriteHeader(http.StatusForbidden)
 		return
 	}
 
-	if response != (Response{}) {
-		w.Header().Add("Content-Type", "application/json")
-		response.Send(w)
+	if params.Get("ssl_check") == "1" {
+		return
 	}
+
+	app := randomizer.NewApp(a.Name, a.StoreFactory(params.Get("channel_id")))
+	result, err := app.Main(strings.Split(params.Get("text"), " "))
+	if err != nil {
+		response{typeEphemeral, err.(randomizer.Error).HelpText()}.Send(w)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	switch result.Type() {
+	case randomizer.ListedGroups, randomizer.ShowedGroup:
+		response{typeEphemeral, result.Message()}.Send(w)
+
+	default:
+		response{typeInChannel, result.Message()}.Send(w)
+	}
+}
+
+type responseType int
+
+const (
+	typeEphemeral responseType = iota + 1
+	typeInChannel
+)
+
+func (t responseType) MarshalText() ([]byte, error) {
+	switch t {
+	case typeEphemeral:
+		return []byte("ephemeral"), nil
+
+	case typeInChannel:
+		return []byte("in_channel"), nil
+	}
+
+	panic(fmt.Errorf("unknown response type code %v", t))
+}
+
+type response struct {
+	Type responseType `json:"response_type"`
+	Text string       `json:"text"`
+}
+
+func (r response) Send(w io.Writer) {
+	if r == (response{}) {
+		return
+	}
+
+	json.NewEncoder(w).Encode(&r)
 }
