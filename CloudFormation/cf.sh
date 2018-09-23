e26a7cc27351dd381782c4df084790b11761f224
Alex Hamlin <alex@alexhamlin.co>
2018-09-22 19:52:09 -0700

    Replace CloudFormation Makefile with a script
    I feel like it's not that easy to represent these steps in a Makefile.
    They're a bit more imperative than Make generally is, especially in
    corner cases like deploying for the first time (when you have to give
    the Slack token override).

---
+++ CloudFormation/cf.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+usage () {
+  cat <<EOF
+cf.sh - Deploy the randomizer to your AWS account using CloudFormation
+
+The AWS CLI must be installed and configured to use this script. For details
+about configuration, see the AWS Documentation:
+
+https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
+
+$0 build
+  (Re)build the Go binary that will be deployed to AWS Lambda.
+
+$0 deploy <stack name> <S3 bucket> [args...]
+  Deploy the randomizer using CloudFormation. This will upload the built Go
+  binary to the provided S3 bucket, then run a deployment.
+
+  Additional arguments are passed directly to "aws cloudformation deploy". In
+  particular, when deploying the stack for the first time, use
+  "--parameter-overrides SlackToken=<token>" to set the token used to
+  authenticate requests from Slack.
+
+$0 get-url <stack name>
+  Print the API Gateway URL that points to the stack with the given name. This
+  requires the "jq" tool.
+
+$0 help
+  Print this message.
+EOF
+}
+
+build () (
+  set -x
+
+  mkdir -p dist
+  CGO_ENABLED=0 GOOS=linux GOARCH=amd64 \
+    go build \
+    -ldflags='-s -w' \
+    -o dist/slack-lambda-handler \
+    ../cmd/slack-lambda-handler
+)
+
+deploy () (
+  if [ ! -d dist ]; then
+    echo "must build the handler binary before deployment" 1>&2
+    return 1
+  fi
+
+  stack_name="$1"
+  s3_bucket="$2"
+  shift 2
+
+  set -x
+
+  aws cloudformation package \
+    --template-file Template.yaml \
+    --output-template-file Package.yaml \
+    --s3-bucket "$s3_bucket"
+
+  aws cloudformation deploy \
+    --template-file Package.yaml \
+    --capabilities CAPABILITY_IAM \
+    --stack-name "$stack_name" \
+    --no-fail-on-empty-changeset \
+    "$@"
+)
+
+get-url () {
+  if ! type jq >/dev/null 2>&1; then
+    echo "jq is required to get the deployment URL" 1>&2
+    return 1
+  fi
+
+  stack_name="$1"
+
+  region="$(aws configure get region)"
+  aws cloudformation describe-stack-resources --stack-name "$stack_name" \
+    | jq -r -f <(cat <<JQ
+[
+  .StackResources[]
+  | select(.ResourceType | contains("AWS::ApiGateway"))
+  | { (.ResourceType | split("::")[2]): .PhysicalResourceId }
+]
+| add
+| "https://" + .RestApi + ".execute-api.$region.amazonaws.com/" + .Stage + "/"
+JQ
+)
+}
+
+cmd="${1:-help}"
+[ "$#" -gt 0 ] && shift
+
+case "$cmd" in
+  build)
+    build "$@"
+    ;;
+  deploy)
+    deploy "$@"
+    ;;
+  get-url)
+    get-url "$@"
+    ;;
+  help)
+    usage
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+esac
