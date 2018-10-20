012a84c4541b59d2d061e1269acb9b0bdde09794
Alex Hamlin <alex@alexhamlin.co>
2018-10-20 12:37:34 -0700

    Fill in unit tests for randomizer core
    This covers the majority of the randomizer's functionality, with only a
    few gaps related to edge cases.

--- pkg/randomizer/main_test.go
+++ pkg/randomizer/main_test.go
@@ -70,10 +70,6 @@ func isError(contains string) validator {
 
 		rerr := err.(Error)
 
-		if !strings.HasPrefix(rerr.HelpText(), "Whoops") {
-			t.Error(`error help text missing standard prefix "Whoops"`)
-		}
-
 		if !strings.Contains(rerr.HelpText(), contains) {
 			t.Errorf("error help text missing substring %q", contains)
 		}
@@ -87,16 +83,146 @@ var testCases = []struct {
 	validIf       validator
 	expectedStore mockStore
 }{
+	// Basic functionality
+
+	{
+		description: "providing no options",
+		args:        []string{},
+		validIf:     isError("need at least two options"),
+	},
+
 	{
 		description: "choosing one of a set of options",
 		args:        []string{"three", "two", "one"},
 		validIf:     isResult(Selection, "*one*"),
 	},
 
+	// Selecting from groups
+
 	{
-		description: "providing no options",
-		args:        []string{},
-		validIf:     isError("need at least two options"),
+		description: "choosing one option from a group",
+		store:       mockStore{"test": {"three", "two", "one"}},
+		args:        []string{"+test"},
+		validIf:     isResult(Selection, "*one*"),
+	},
+
+	{
+		description: "combining groups with literal options",
+		store:       mockStore{"test": {"three", "two", "one"}},
+		args:        []string{"+test", "four"},
+		validIf:     isResult(Selection, "*four*"),
+	},
+
+	{
+		description: "combining multiple groups",
+		store: mockStore{
+			"first":  {"one", "two", "three"},
+			"second": {"four", "five", "six"},
+		},
+		args:    []string{"+first", "+second"},
+		validIf: isResult(Selection, "*five*"),
+	},
+
+	{
+		description: "choosing from a group that does not exist",
+		args:        []string{"+test"},
+		validIf:     isError(`couldn't find the "test" group`),
+	},
+
+	{
+		description: "removing an option from consideration",
+		store:       mockStore{"test": {"three", "two", "one"}},
+		args:        []string{"+test", "-one"},
+		validIf:     isResult(Selection, "*three*"),
+	},
+
+	{
+		description: "removing an option that does not exist",
+		store:       mockStore{"test": {"three", "two", "one"}},
+		args:        []string{"+test", "-four"},
+		validIf:     isError(`"four" wasn't available for me to remove`),
+	},
+
+	// Multiple selections
+
+	{
+		description: "choosing multiple options",
+		args:        []string{"-n", "2", "one", "two", "three", "four"},
+		validIf:     isResult(Selection, "*four*", "*one*"),
+	},
+
+	{
+		description: "choosing all options",
+		args:        []string{"-n", "all", "one", "two", "three", "four"},
+		validIf:     isResult(Selection, "*four*", "*one*", "*three*", "*two*"),
+	},
+
+	{
+		description: "choosing too few options",
+		args:        []string{"-n", "0", "one", "two"},
+		validIf:     isError("can't pick less than one option"),
+	},
+
+	{
+		description: "choosing too many options",
+		args:        []string{"-n", "3", "one", "two"},
+		validIf:     isError("can't pick more options than I was given"),
+	},
+
+	{
+		description: "non-integer options count",
+		args:        []string{"-n", "2.1", "one", "two"},
+		validIf:     isError("helps you pick options randomly"), // usage message
+	},
+
+	{
+		description: "invalid options count",
+		args:        []string{"-n", "wat", "one", "two"},
+		validIf:     isError("helps you pick options randomly"), // usage message
+	},
+
+	// Group CRUD operations
+
+	{
+		description: "listing groups",
+		store:       mockStore{"first": {"one"}, "second": {"two"}},
+		args:        []string{"-list"},
+		validIf:     isResult(ListedGroups, "• first", "• second"),
+	},
+
+	{
+		description: "listing groups when there are none",
+		args:        []string{"-list"},
+		validIf:     isResult(ListedGroups, "No groups are available"),
+	},
+
+	{
+		description: "showing a group",
+		store:       mockStore{"test": {"one", "two", "three"}},
+		args:        []string{"-show", "test"},
+		validIf:     isResult(ShowedGroup, "• one", "• two", "• three"),
+	},
+
+	{
+		description: "showing a group that does not exist",
+		args:        []string{"-show", "test"},
+		validIf:     isError("couldn't find that group"),
+	},
+
+	{
+		description:   "saving a group",
+		store:         mockStore{},
+		args:          []string{"-save", "test", "one", "two"},
+		validIf:       isResult(SavedGroup, `The "test" group was saved`, "• one", "• two"),
+		expectedStore: mockStore{"test": {"one", "two"}},
+	},
+
+	{
+		description:   "deleting a group",
+		store:         mockStore{"test": {"one", "two"}},
+		args:          []string{"-delete", "test"},
+		validIf:       isResult(DeletedGroup, `The "test" group was deleted`),
+		expectedStore: mockStore{},
 	},
 }
 
