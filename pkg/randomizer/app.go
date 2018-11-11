afaff3953f2af18403239bbfb40f57273343255c
Alex Hamlin <alex@alexhamlin.co>
2018-11-10 22:55:25 -0800

    Separate randomizer.App from basic types
    Separating the implementation should make it a bit easier to read, I
    hope. Though perhaps I should only have separated randomizer.Error.
    We'll see how this goes.

--- pkg/randomizer/app.go
+++ pkg/randomizer/app.go
@@ -1,4 +1,4 @@
-package randomizer // import "go.alexhamlin.co/randomizer/pkg/randomizer"
+package randomizer
 
 import (
 	"bytes"
@@ -13,70 +13,16 @@ import (
 	"github.com/pkg/errors"
 )
 
-// ResultType represents the type of successful result returned by the
-// randomizer.
-type ResultType int
+// Store represents an object that provides persistence for "groups" of
+// options.
+type Store interface {
+	List() (groups []string, err error)
 
-const (
-	// Selection indicates that the randomizer made a random selection from input
-	// options.
-	Selection ResultType = iota
-	// ShowedHelp indicates that the randomizer displayed its help output.
-	ShowedHelp
-	// ListedGroups indicates that a group list was successfully obtained.
-	ListedGroups
-	// ShowedGroup indicates that the options of a single group were successfully obtained.
-	ShowedGroup
-	// SavedGroup indicates that a group was successfully saved.
-	SavedGroup
-	// DeletedGroup indicates that a group was successfully deleted.
-	DeletedGroup
-)
+	Get(group string) (options []string, err error)
 
-// Result represents a successful randomizer operation.
-type Result struct {
-	resultType ResultType
-	message    string
-}
+	Put(group string, options []string) error
 
-// Type returns the type of this result.
-func (r Result) Type() ResultType {
-	return r.resultType
-}
-
-// Message returns the user-friendly output associated with this result.
-func (r Result) Message() string {
-	return r.message
-}
-
-// Error represents an error encountered by the randomizer. It includes
-// friendly help messages that can be displayed directly to users when errors
-// occur, along with an underlying developer-friendly error that may be useful
-// for debugging.
-type Error struct {
-	cause    error
-	helpText string
-}
-
-func (e Error) Error() string {
-	return e.cause.Error()
-}
-
-// Cause returns the underlying developer-friendly error that represents this
-// usage error.
-func (e Error) Cause() error {
-	return e.cause
-}
-
-// HelpText returns user-friendly help text associated with this error. While
-// the underlying error is more suitable for developer use, the help text may
-// be displayed directly to a user.
-func (e Error) HelpText() string {
-	if e.helpText != "" {
-		return e.helpText
-	}
-
-	return fmt.Sprintf("Whoops, I had a problem… %v", e.cause)
+	Delete(group string) error
 }
 
 // App represents a randomizer app that can accept commands.
@@ -86,15 +32,6 @@ type App struct {
 
 	// May be overridden in unit tests for more predictable behavior
 	shuffle func([]string)
-}
-
-// Store represents an object that provides persistence for "groups" of
-// options.
-type Store interface {
-	List() ([]string, error)
-	Get(name string) ([]string, error)
-	Put(name string, options []string) error
-	Delete(name string) error
 }
 
 // NewApp returns an App.
---
+++ pkg/randomizer/randomizer.go
@@ -0,0 +1,69 @@
+package randomizer // import "go.alexhamlin.co/randomizer/pkg/randomizer"
+
+import "fmt"
+
+// ResultType represents the type of successful result returned by the
+// randomizer.
+type ResultType int
+
+const (
+	// Selection indicates that the randomizer made a random selection from input
+	// options.
+	Selection ResultType = iota
+	// ShowedHelp indicates that the randomizer displayed its help output.
+	ShowedHelp
+	// ListedGroups indicates that a group list was successfully obtained.
+	ListedGroups
+	// ShowedGroup indicates that the options of a single group were successfully obtained.
+	ShowedGroup
+	// SavedGroup indicates that a group was successfully saved.
+	SavedGroup
+	// DeletedGroup indicates that a group was successfully deleted.
+	DeletedGroup
+)
+
+// Result represents a successful randomizer operation.
+type Result struct {
+	resultType ResultType
+	message    string
+}
+
+// Type returns the type of this result.
+func (r Result) Type() ResultType {
+	return r.resultType
+}
+
+// Message returns the user-friendly output associated with this result.
+func (r Result) Message() string {
+	return r.message
+}
+
+// Error represents an error encountered by the randomizer. It includes
+// friendly help messages that can be displayed directly to users when errors
+// occur, along with an underlying developer-friendly error that may be useful
+// for debugging.
+type Error struct {
+	cause    error
+	helpText string
+}
+
+func (e Error) Error() string {
+	return e.cause.Error()
+}
+
+// Cause returns the underlying developer-friendly error that represents this
+// usage error.
+func (e Error) Cause() error {
+	return e.cause
+}
+
+// HelpText returns user-friendly help text associated with this error. While
+// the underlying error is more suitable for developer use, the help text may
+// be displayed directly to a user.
+func (e Error) HelpText() string {
+	if e.helpText != "" {
+		return e.helpText
+	}
+
+	return fmt.Sprintf("Whoops, I had a problem… %v", e.cause)
+}
