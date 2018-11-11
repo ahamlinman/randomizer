c38396b6eeb6af272a28c28fe5b14d60d5462168
Alex Hamlin <alex@alexhamlin.co>
2018-11-10 21:22:00 -0800

    Document and clean up slashy flag parser
    Just some assorted changes to hopefully clean up the code a bit. Ensure
    consistent use of terminology (arguments, flags, values), shuffle some
    helpers toward the bottom of the file, simplify control flow, etc.

--- pkg/randomizer/options.go
+++ pkg/randomizer/options.go
@@ -27,8 +27,18 @@ type options struct {
 	Count int
 }
 
+// flagHandler is a type for functions that can parse a flag and its value(s)
+// from an argument list into an options struct.
+//
+// The argument slice provided to the handler starts at the argument containing
+// the flag itself. If the returned error is nil, the returned int is the total
+// number of arguments (1 or more) consumed by parsing this flag and its
+// value(s).
 type flagHandler func(*options, []string) (int, error)
 
+// operationFlagHandlers represents "operation" flags, which must appear at the
+// start of the argument list. Operations are alternate modes of behavior that
+// do not involve randomly selecting from lists of items.
 var operationFlagHandlers = map[string]flagHandler{
 	"/help":   (*options).parseHelp,
 	"/list":   (*options).parseList,
@@ -37,6 +47,9 @@ var operationFlagHandlers = map[string]flagHandler{
 	"/delete": (*options).parseDelete,
 }
 
+// modifierFlagHandlers represents "modifier" flags, which may appear anywhere
+// in the argument list. Modifiers affect the behavior of an operation,
+// particularly the normal operation of selecting randomly from a list.
 var modifierFlagHandlers = map[string]flagHandler{
 	"/n": (*options).parseN,
 }
@@ -60,12 +73,14 @@ func parseArgs(args []string) (options, error) {
 		return nil
 	}
 
+	// Consume an operation flag, if we have one.
 	if handler := operationFlagHandlers[args[0]]; handler != nil {
 		if err := consumeFlag(handler); err != nil {
 			return opts, err
 		}
 	}
 
+	// Process all remaining arguments, consuming modifiers as they appear.
 	for len(args) > 0 {
 		if handler := modifierFlagHandlers[args[0]]; handler != nil {
 			if err := consumeFlag(handler); err != nil {
@@ -74,31 +89,26 @@ func parseArgs(args []string) (options, error) {
 			continue
 		}
 
-		var nonFlagArgs []string
-		nonFlagArgs, args = splitArgsAtNextModifier(args)
-		opts.Args = append(opts.Args, nonFlagArgs...)
+		var nonFlags []string
+		nonFlags, args = splitArgsAtNextModifier(args)
+		opts.Args = append(opts.Args, nonFlags...)
 	}
 
 	return opts, nil
 }
 
 func splitArgsAtNextModifier(args []string) (nonFlags []string, rest []string) {
-	// Start by assuming that none of the remaining arguments have flags.
-	nonFlags = args
-	// rest = nil
-
-	// Run through the array and check this assumption.
+	// Look for modifier flags in the argument list. If we find one, split the
+	// list so it becomes the first item in rest.
 	for i, arg := range args {
-		if _, ok := modifierFlagHandlers[arg]; !ok {
-			continue
+		if _, ok := modifierFlagHandlers[arg]; ok {
+			nonFlags, rest = args[:i], args[i:]
+			return
 		}
-
-		// It's wrong, so overwrite the result based on the flag we've identified.
-		nonFlags = args[:i]
-		rest = args[i:]
-		break
 	}
 
+	// Otherwise, all arguments are non-flag arguments.
+	nonFlags, rest = args, nil
 	return
 }
 
@@ -112,46 +122,25 @@ func (opts *options) parseList(_ []string) (int, error) {
 	return 1, nil
 }
 
-func (options) parseFlagValue(args []string) (string, error) {
-	if len(args) < 2 {
-		return "", Error{
-			cause:    errors.Errorf("%q flag requires an argument", args[0]),
-			helpText: fmt.Sprintf("Whoops, %q requires an argument!", args[0]),
-		}
-	}
-
-	return args[1], nil
-}
-
-func (opts *options) parseOperation(op operation, args []string) (consumed int, err error) {
-	consumed = 2
-
-	value, err := opts.parseFlagValue(args)
-	if err != nil {
-		return
-	}
-
-	opts.Operation = op
-	opts.Operand = value
-	return
-}
-
 func (opts *options) parseShow(args []string) (int, error) {
-	return opts.parseOperation(showGroup, args)
+	opts.Operation = showGroup
+	return 2, opts.parseOperand(args)
 }
 
 func (opts *options) parseSave(args []string) (int, error) {
-	return opts.parseOperation(saveGroup, args)
+	opts.Operation = saveGroup
+	return 2, opts.parseOperand(args)
 }
 
 func (opts *options) parseDelete(args []string) (int, error) {
-	return opts.parseOperation(deleteGroup, args)
+	opts.Operation = deleteGroup
+	return 2, opts.parseOperand(args)
 }
 
 func (opts *options) parseN(args []string) (consumed int, err error) {
 	consumed = 2
 
-	value, err := opts.parseFlagValue(args)
+	value, err := parseFlagValue(args)
 	if err != nil {
 		return
 	}
@@ -169,4 +158,20 @@ func (opts *options) parseN(args []string) (consumed int, err error) {
 		}
 	}
 	return
+}
+
+func (opts *options) parseOperand(args []string) (err error) {
+	opts.Operand, err = parseFlagValue(args)
+	return
+}
+
+func parseFlagValue(args []string) (string, error) {
+	if len(args) < 2 {
+		return "", Error{
+			cause:    errors.Errorf("%q flag requires an argument", args[0]),
+			helpText: fmt.Sprintf("Whoops, %q requires an argument!", args[0]),
+		}
+	}
+
+	return args[1], nil
 }
