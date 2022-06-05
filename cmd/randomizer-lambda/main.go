bc329c18b672c087bfb0cf353bba8b055002d6d8
Alex Hamlin <alex@alexhamlin.co>
2022-06-04 18:11:27 -0700

    Support 1.0 and 2.0 Lambda HTTP proxy formats
    This should allow the function to support a Lambda Function URL and an
    API Gateway at the same time, so that we can introduce support for the
    former without breaking backwards compatibility for existing deployments
    with known webhook URLs.

--- cmd/randomizer-lambda/main.go
+++ cmd/randomizer-lambda/main.go
@@ -8,8 +8,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
@@ -31,10 +33,40 @@ func main() {
 		os.Exit(2)
 	}
 
-	app := slack.App{
-		TokenProvider: tokenProvider,
-		StoreFactory:  storeFactory,
-		DebugWriter:   os.Stderr,
+	handler := handler{
+		slack.App{
+			TokenProvider: tokenProvider,
+			StoreFactory:  storeFactory,
+			DebugWriter:   os.Stderr,
+		},
 	}
-	lambda.Start(httpadapter.New(app).ProxyWithContext)
+	lambda.Start(handler.Handle)
+}
+
+type handler struct {
+	slack.App
+}
+
+func (h handler) Handle(ctx context.Context, event json.RawMessage) (any, error) {
+	var version struct {
+		Version string `json:"version"`
+	}
+	json.Unmarshal(event, &version) // Assume that Lambda's JSON input is valid.
+
+	if strings.Split(version.Version, ".")[0] == "2" {
+		return proxyHTTP(ctx, event, httpadapter.NewV2(h.App).ProxyWithContext)
+	}
+	return proxyHTTP(ctx, event, httpadapter.New(h.App).ProxyWithContext)
+}
+
+func proxyHTTP[In, Out any](
+	ctx context.Context,
+	event json.RawMessage,
+	httpHandler func(context.Context, In) (Out, error),
+) (any, error) {
+	var input In
+	if err := json.Unmarshal(event, &input); err != nil {
+		return nil, err
+	}
+	return httpHandler(ctx, input)
 }
