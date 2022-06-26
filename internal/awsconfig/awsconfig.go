684e61719f381ac0b5123b4401e93b1b7f279334
Alex Hamlin <alex@alexhamlin.co>
2022-06-25 19:33:05 -0700

    Make embedded CAs optional but default on Lambda
    With the switch from images built by zeroimage to the provided.al2
    runtime, we can in theory use the root CAs provided by the runtime.
    However, this has such a significant effect on cold start performance
    that I'd rather stick with the embedding approach. See comments for
    additional details.

--- internal/awsconfig/awsconfig.go
+++ internal/awsconfig/awsconfig.go
@@ -24,11 +24,18 @@ const (
 
 type Option = func(*config.LoadOptions) error
 
-// New creates a new AWS client configuration using pinned TLS roots and
-// reasonable default settings for timeouts and retries.
+// New creates a new AWS client configuration using reasonable default settings
+// for timeouts and retries.
 func New(ctx context.Context, extraOptions ...Option) (aws.Config, error) {
 	transport := http.DefaultTransport.(*http.Transport).Clone()
-	transport.TLSClientConfig = &tls.Config{RootCAs: getTLSRootPool()}
+
+	// This option is enabled by default in the recommended Lambda deployment
+	// configuration due to the significant reduction in cold start latency. It
+	// can be enabled for standard server deployments if desired, but is likely
+	// far less beneficial.
+	if useEmbeddedRoots := os.Getenv("AWS_CLIENT_EMBEDDED_TLS_ROOTS"); useEmbeddedRoots == "1" {
+		transport.TLSClientConfig = &tls.Config{RootCAs: getRootCAPool()}
+	}
 
 	client := &http.Client{
 		Timeout:   DefaultTimeout,
@@ -52,8 +59,8 @@ func New(ctx context.Context, extraOptions ...Option) (aws.Config, error) {
 
 	// WARNING: X-Ray tracing will panic if the context passed to AWS operations
 	// is not already associated with an open X-Ray segment. That means that as of
-	// this writing this option is only safe to use on AWS Lambda. Other clients
-	// should avoid setting it.
+	// this writing this option is only safe to use on AWS Lambda. Standard server
+	// deployments should avoid setting it.
 	if useXRay := os.Getenv("AWS_CLIENT_XRAY_TRACING"); useXRay == "1" {
 		xrayawsv2.AWSV2Instrumentor(&cfg.APIOptions)
 	}
@@ -61,20 +68,30 @@ func New(ctx context.Context, extraOptions ...Option) (aws.Config, error) {
 	return cfg, errors.Wrap(err, "loading AWS config")
 }
 
+// tlsRootsPEM contains PEM-encoded X.509 certificates for the root CAs used by
+// AWS service endpoints.
+//
+// When the randomizer runs on AWS Lambda in the recommended configuration, this
+// limited set of certificates is so much cheaper to parse than the full set of
+// roots trusted by Amazon Linux 2 that it cuts invocation time on cold starts
+// approximately in half, specifically by around 500ms. This is a large enough
+// difference for a human to notice, and accounts for about 15% of the 3 second
+// response time limit that Slack imposes on slash commands.
+//
 //go:generate ./refresh-tls-roots.sh
+//go:embed cert.pem
+var tlsRootsPEM []byte
 
 var (
-	//go:embed cert.pem
-	tlsRootsPEM  []byte
 	tlsRoots     *x509.CertPool
 	initTLSRoots sync.Once
 )
 
-func getTLSRootPool() *x509.CertPool {
+func getRootCAPool() *x509.CertPool {
 	initTLSRoots.Do(func() {
 		tlsRoots = x509.NewCertPool()
 		if !tlsRoots.AppendCertsFromPEM(tlsRootsPEM) {
-			panic("failed to initialize TLS roots for DynamoDB client")
+			panic("failed to initialize Amazon TLS root pool")
 		}
 	})
 	return tlsRoots
