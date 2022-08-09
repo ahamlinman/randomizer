05eb0d89c4deafa06454e9eacdd6fbba543d93c0
Alex Hamlin <alex@alexhamlin.co>
2022-08-08 23:52:04 -0700

    Fix permissions on DynamoDB test volume
    I guess the DynamoDB Docker image must have been using root before?
    
    I also returned `set -e` to the script after getting way too worked up
    about people expressing online opinions a few months ago. This is a
    super basic 20-line script, the edge cases it can possibly run into are
    quite limited.

--- dynamodb-docker.sh
+++ dynamodb-docker.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
+set -ex
 
 docker="${DOCKER:-docker}"
+dynamodb_image="${DYNAMODB_IMAGE:-amazon/dynamodb-local:latest}"
 volume_name="${VOLUME_NAME:-dynamodb-randomizer}"
 container_name="${CONTAINER_NAME:-dynamodb-randomizer}"
 host_port="${HOST_PORT:-8000}"
 
 if ! "$docker" volume inspect "$volume_name" >/dev/null 2>&1; then
-  (set -x; "$docker" volume create "$volume_name") || exit $?
+  "$docker" volume create "$volume_name"
+  "$docker" run --rm -v "$volume_name":/var/lib/randomizer -u 0:0 --entrypoint "" "$dynamodb_image" \
+    chown dynamodblocal:dynamodblocal /var/lib/randomizer
 fi
 
-set -x
 "$docker" run \
   --rm -d --name "$container_name" \
   -p "$host_port":8000 \
   -v "$volume_name":/var/lib/randomizer \
-  amazon/dynamodb-local -jar DynamoDBLocal.jar -dbPath /var/lib/randomizer
+  "$dynamodb_image" -jar DynamoDBLocal.jar -dbPath /var/lib/randomizer
