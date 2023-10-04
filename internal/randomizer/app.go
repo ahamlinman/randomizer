9bbe2eb4a18237c65a4d9cc2da230c17df8bd0f2
Alex Hamlin <alex@alexhamlin.co>
2023-10-03 23:59:28 -0700

    Silly things with app.go

--- internal/randomizer/app.go
+++ internal/randomizer/app.go
@@ -9,31 +9,29 @@ import (
 // Store represents an object that provides persistence for "groups" of
 // options.
 type Store interface {
-	// List should return a list of all available groups. If no groups have been
-	// saved, an empty list should be returned.
+	// List returns the names of all available groups. If no groups have been
+	// saved, it returns an empty list with a nil error.
 	List(ctx context.Context) (groups []string, err error)
 
-	// Get should return the list of options in the named group. If the group
-	// does not exist, an empty list should be returned.
+	// Get returns the list of options in the named group. If the group does not
+	// exist, it returns an empty list with a nil error.
 	Get(ctx context.Context, group string) (options []string, err error)
 
-	// Put should save the provided options as a named group, completely
-	// overwriting any previous group of that name.
+	// Put saves the provided options as a named group, overwriting any previous
+	// group with that name.
 	Put(ctx context.Context, group string, options []string) error
 
-	// Delete should ensure that the named group no longer exists, returning true
-	// or false to indicate whether the group existed prior to this deletion
+	// Delete ensures that the named group no longer exists, returning true or
+	// false to indicate whether the group existed prior to this deletion
 	// attempt.
 	Delete(ctx context.Context, group string) (bool, error)
 }
 
 // App represents a randomizer app that can accept commands.
 type App struct {
-	name  string
-	store Store
-
-	// May be overridden in unit tests for more predictable behavior
-	shuffle func([]string)
+	name    string
+	store   Store
+	shuffle func([]string) // Overridden in tests for predictable behavior
 }
 
 // NewApp returns an App.
@@ -62,26 +60,17 @@ func (a App) Main(ctx context.Context, args []string) (Result, error) {
 	if err != nil {
 		return Result{}, err
 	}
-
 	handler := appHandlers[request.Operation]
 	return handler(a, request)
 }
 
 type appHandler func(App, request) (Result, error)
 
-// appHandlers maps every possible mode of operation for the randomizer to a
-// handler method, which processes a request from the user and returns an
-// appropriate result.
 var appHandlers = map[operation]appHandler{
-	// selection.go
+	showHelp:      App.showHelp,
 	makeSelection: App.makeSelection,
-
-	// help.go
-	showHelp: App.showHelp,
-
-	// groups.go
-	listGroups:  App.listGroups,
-	showGroup:   App.showGroup,
-	saveGroup:   App.saveGroup,
-	deleteGroup: App.deleteGroup,
+	listGroups:    App.listGroups,
+	showGroup:     App.showGroup,
+	saveGroup:     App.saveGroup,
+	deleteGroup:   App.deleteGroup,
 }
