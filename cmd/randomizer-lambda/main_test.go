5d7fba20242a5de281d0493e1664cfcd21482641
Alex Hamlin <alex@alexhamlin.co>
2025-03-02 17:30:31 -0800

    Add a test for banned packages in randomizer-lambda
    This is to help keep me honest about my recent optimization work, and is
    again influenced by the fact that the Cobra change included a test.
    
    Note that I don't just match MethodByName like that test does. We have
    some MethodByName calls in the final binary, which are fine because the
    compiler can statically determine the methods being looked up. It's only
    when you call that method with a dynamic value that you block all
    possibility of dead code elimination.

---
+++ cmd/randomizer-lambda/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+func TestBannedPackages(t *testing.T) {
+	// According to https://github.com/spf13/cobra/blob/v1.9.0/cobra_test.go#L247,
+	// `go tool nm` doesn't work on Windows.
+	if runtime.GOOS == "windows" {
+		t.SkipNow()
+	}
+
+	const name = "randomizer-lambda"
+	binpath := filepath.Join(t.TempDir(), name)
+	build := exec.Command("go", "build", "-tags=lambda.norpc,grpcnotrace", "-o", binpath, ".")
+	build.Stdout, build.Stderr = os.Stdout, os.Stderr
+	if err := build.Run(); err != nil {
+		t.Fatalf("failed to build: %v", err)
+	}
+
+	var nmout strings.Builder
+	nm := exec.Command("go", "tool", "nm", binpath)
+	nm.Stdout, nm.Stderr = &nmout, os.Stderr
+	if err := nm.Run(); err != nil {
+		t.Fatalf("failed to run go tool nm: %v", err)
+	}
+
+	if strings.Contains(nmout.String(), "T text/template.") {
+		t.Errorf("%s imports text/template, blocking dead code elimination", name)
+	}
+	if strings.Contains(nmout.String(), "T html/template.") {
+		t.Errorf("%s imports html/template, blocking dead code elimination", name)
+	}
+}
